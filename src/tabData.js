8c8bfa16562178115a7a4d87385f9a07f3991bf7
Fae-new <faluyiaji@gmail.com>
2022-06-13 17:12:58 +0100

    finishing touches

--- src/tabData.js
+++ src/tabData.js
@@ -1,24 +1,20 @@
-const tabData=[
+const tabData = [{
+        mainText: ' Bookmark in one click',
+        subText: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+        image: 'images/illustration-features-tab-1.svg'
+    },
+
     {
-        mainText:' Bookmark in one click',
-        subText:'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
-        image:'images/illustration-features-tab-1.svg'
-            },
+        mainText: ' Intelligent search ',
+        subText: ' Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+        image: 'images/illustration-features-tab-2.svg'
+    },
 
- {
-     mainText:' Intelligent search ',
-    subText:' Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
-    image:'images/illustration-features-tab-2.svg'
-                    },
-                    
     {
-     mainText:' Share your bookmarks',
-    subText:' Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
-    image:'images/illustration-features-tab-3.svg'
-                   }
-
-
-
+        mainText: ' Share your bookmarks',
+        subText: ' Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+        image: 'images/illustration-features-tab-3.svg'
+    }
 
 ]
 
